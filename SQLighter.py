ccc7c2c6630d463ab4623940397105d357bbdfd4
Никита Ишутов <nikitaisutov@MacBook-Pro-Nikita-2.local>
2021-02-27 16:04:09 +0300

    второй пошел

--- SQLighter.py
+++ SQLighter.py
@@ -13,7 +13,7 @@ class SQLighter:
         добавляет категорию
         """
         self.cursor.execute(
-            f"INSERT INTO traning_category (name) VALUES('{category_name}')")
+            f"INSERT INTO training_category (name) VALUES('{category_name}')")
         self.connection.commit()
 
     def send_progres(self, user_id, date, completed):
@@ -47,7 +47,7 @@ class SQLighter:
         """
         users = self.cursor.execute(
             f"SELECT * FROM users WHERE sber_id = '{sber_id}'").fetchall()
-        result = dict()
+        result = []
         for i in range(len(users)):
             res = dict()
             res["id"] = users[i][0]
@@ -56,7 +56,7 @@ class SQLighter:
             res["age"] = users[i][3]
             res["gender"] = users[i][4]
             res["active"] = users[i][5]
-            result[str(users[i][0])] = res
+            result.append(res)
 
         return result
 
@@ -65,12 +65,13 @@ class SQLighter:
         Достает все категории
         """
         category = self.cursor.execute(
-            f"SELECT * FROM traning_category").fetchall()
-        result = dict()
+            f"SELECT * FROM training_category").fetchall()
+        result = []
         for i in range(len(category)):
             res = dict()
+            res["id"] = category[i][0]
             res["name"] = category[i][1]
-            result[str(category[i][0])] = res
+            result.append(res)
         return result
 
     def get_progres_by_user(self, user_id):
@@ -79,14 +80,14 @@ class SQLighter:
         """
         progres = self.cursor.execute(
             f"SELECT * FROM progress WHERE user_id = '{user_id}'").fetchall()
-        result = dict()
+        result = []
         for progres_day in progres:
             res = dict()
             res["id"] = progres_day[0]
             res["user_id"] = progres_day[1]
             res["date"] = progres_day[2]
             res["completed"] = progres_day[3]
-            result[str(progres_day[0])] = res
+            result.append(res)
         return result
 
     def get_category_by_id(self, category_id):
@@ -94,12 +95,13 @@ class SQLighter:
         Достает категорию по айди
         """
         category = self.cursor.execute(
-            f"SELECT * FROM traning_category WHERE id = '{category_id}'").fetchall()
-        result = dict()
+            f"SELECT * FROM training_category WHERE id = '{category_id}'").fetchall()
+        result = []
         for i in range(len(category)):
             res = dict()
+            res["id"]=category[i][0]
             res["name"] = category[i][1]
-            result[str(category[i][0])] = res
+            result.append(res)
         return result
 
     def get_all_group(self):
@@ -107,12 +109,15 @@ class SQLighter:
         Достает все группы
         """
         group = self.cursor.execute(
-            f"SELECT * FROM traning_group").fetchall()
-        result = dict()
+            f"SELECT * FROM training_group").fetchall()
+        result = []
         for i in range(len(group)):
             res = dict()
+            res['id'] = group[i][0]
             res["name"] = group[i][1]
-            result[str(group[i][0])] = res
+            res["short_discription"] = group[i][2]
+            res["discription"] = group[i][3]
+            result.append(res)
         return result
 
     def get_exircices_from_group(self, group_id):
@@ -121,21 +126,21 @@ class SQLighter:
         """
 
         exircices_id = self.cursor.execute(
-            f"SELECT traning_id FROM traning_traning_group WHERE traning_group_id = '{group_id}'").fetchall()
-        result = dict()
+            f"SELECT training_id FROM training_training_group WHERE training_group_id = '{group_id}'").fetchall()
+        result = []
         for exircices in exircices_id:
             ex_id = exircices[0]
             ex = self.cursor.execute(
-                f"SELECT * FROM traning WHERE id = '{ex_id}'").fetchall()
+                f"SELECT * FROM training WHERE id = '{ex_id}'").fetchall()
             res = dict()
             res["id"] = ex[0][0]
             res["category"] = self.cursor.execute(
-                f"SELECT name FROM traning_category WHERE id = '{ex[0][1]}'").fetchall()[0][0]
+                f"SELECT name FROM training_category WHERE id = '{ex[0][1]}'").fetchall()[0][0]
             res["name"] = ex[0][2]
             res["discription"] = ex[0][3]
             res["photo"] = ex[0][4]
             res["time"] = ex[0][5]
-            result[str(ex[0][0])] = res
+            result.append(res)
         return result
 
     def get_sber_id(self, sber_id):
@@ -153,13 +158,13 @@ class SQLighter:
         """
         phras = self.cursor.execute(
             f"SELECT * FROM motivation WHERE id = '{id}'").fetchall()
-        result = dict()
+        result = []
         res = dict()
         res["id"] = phras[0][0]
         res["name"] = phras[0][1]
         res["discription"] = phras[0][2]
         res["author"] = phras[0][3]
-        result[str(phras[0][0])]  = res
+        result.append(res)
         return result
 
 
